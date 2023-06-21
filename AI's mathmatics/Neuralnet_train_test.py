664234de55e92c910b9da3e5ccfa32b347a943a5
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2023-06-21 15:21:55 +0900

    Update Neuralnet_train_test.py

--- AI's mathmatics/Neuralnet_train_test.py
+++ AI's mathmatics/Neuralnet_train_test.py
@@ -6,6 +6,7 @@ import csv
 
 torch.manual_seed(0)
 
+
 class MyDataLoader:
     def __init__(self, *file_paths):
         self.file_paths = file_paths
@@ -18,12 +19,13 @@ class MyDataLoader:
                 self.data.append(np.array(data, float))
         return [torch.from_numpy(data.T).to(torch.float32) for data in self.data]
 
+
 class MyModel(nn.Module):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.fc1 = nn.Linear(420, 200)  # First layer, with 200 nodes
-        self.fc2 = nn.Linear(200, 100)  # Second layer, with 100 nodes
-        self.fc3 = nn.Linear(100, 10)  # Third layer, with 10 nodes
+        self.fc1 = nn.Linear(420, 200)
+        self.fc2 = nn.Linear(200, 100)
+        self.fc3 = nn.Linear(100, 10)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
@@ -31,24 +33,41 @@ class MyModel(nn.Module):
         x = self.fc3(x)
         return x
 
+
 class MyTrainer:
-    def __init__(self, model, input_data, input_data2, test_data, num_epochs=100, learning_rate=0.01):
+    def __init__(self, model, input_data, input_data2, test_data, num_epochs=6000, learning_rate=0.001, patience=10):
         self.model = model
         self.input_data = input_data
         self.input_data2 = input_data2
         self.test_data = test_data
         self.num_epochs = num_epochs
+        self.patience = patience
         self.criterion = nn.MSELoss()
-        self.optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+        self.optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        self.best_loss = np.inf
+        self.early_stopping_counter = 0
+        self.current_loss = None
 
     def train(self):
         for epoch in range(self.num_epochs):
             output_data = self.model(self.input_data)
             loss = self.criterion(output_data, self.input_data2)
+            self.current_loss = loss.item()
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-            print(f"Epoch [{epoch+1}/{self.num_epochs}], Loss: {loss.item()}")
+
+            if self.current_loss < self.best_loss:
+                self.best_loss = self.current_loss
+                self.early_stopping_counter = 0
+            else:
+                self.early_stopping_counter += 1
+
+            if self.early_stopping_counter >= self.patience:
+                print(f"Early stopping at epoch {epoch + 1}")
+                break
+
+            print(f"Epoch [{epoch + 1}/{self.num_epochs}], Loss: {self.current_loss}")
 
     def test(self):
         self.model.eval()
@@ -56,15 +75,18 @@ class MyTrainer:
             output_data = self.model(self.test_data)
         return output_data
 
+
 if __name__ == "__main__":
     file_paths = ["Prb_data/Prb02data01.csv", "Prb_data/Prb02data02.csv", "Prb_data/Prb02data03.csv"]
     data_loader = MyDataLoader(*file_paths)
     input_data, input_data2, test_data = data_loader.load_data()
 
     model = MyModel()
-    trainer = MyTrainer(model, input_data, input_data2, test_data)
+    trainer = MyTrainer(model, input_data, input_data2, test_data, learning_rate=0.001, patience=20)
     trainer.train()
+    print("Final Loss:", trainer.current_loss)
 
     test_output = trainer.test()
+
     print("Test Output Size:", test_output.size())
     print("Test Output:", test_output)
